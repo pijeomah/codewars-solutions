85679f6af4e66fef47888e7889e04538b61c62d7
pijeomah <promiseij4919@gmail.com>
2022-11-08 05:21:36 +0100

    new challenge added

---
+++ 8kyu/name-shuffler.js
@@ -0,0 +1,22 @@
+// DESCRIPTION:
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// Example(Input --> Output)
+
+// "john McClane" --> "McClane john"
+
+//My Solution
+function nameShuffler(str){
+    //Shuffle It
+    let newArr = str.split(' ')
+    newArr.splice(0,2, newArr[1],newArr[0])
+    let newName = newArr.join(' ')
+    return newName
+    
+  }
+
+  //Best Practice
+  function nameSuffle(str){
+    return str.split(' ').reverse().join(' ')
+  }
+  
