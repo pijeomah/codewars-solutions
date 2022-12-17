44382773be4b28960117341da1b0446fbe25d35c
pijeomah <promiseij4919@gmail.com>
2022-12-17 15:25:53 +0100

    new challenge added

---
+++ 8kyu/first-element.js
@@ -0,0 +1,29 @@
+// DESCRIPTION:
+// Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+// If n == 0 return an empty sequence []
+
+// Examples
+// var arr = ['a', 'b', 'c', 'd', 'e'];
+// first(arr) //=> ['a'];
+// first(arr, 2) //=> ['a', 'b']
+// first(arr, 3) //=> ['a', 'b', 'c'];
+// first(arr, 0) //=> [];
+
+//My solution
+function first(arr, n) {
+    // return arr;
+    let empty = []
+    if(n == null){
+      return [arr[0]]
+    }else if(n > arr.length){
+             return arr
+             }
+    
+  for(let i = 0; i < n; i++){
+    empty.push(arr[i])
+  }
+    return empty
+  }
+  
+  
