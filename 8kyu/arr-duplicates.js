91df83135abc600613a16a9364dc558db8addaa4
pijeomah <promiseij4919@gmail.com>
2022-12-23 22:06:03 +0100

    new challenge added

---
+++ 8kyu/arr-duplicates.js
@@ -0,0 +1,16 @@
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+
+
+
+function distinct(a) {
+    let unique =[]
+    a.forEach(el => {
+      if(!unique.includes(el)){
+        unique.push(el)
+      }
+    })
+    return unique;
+  }
