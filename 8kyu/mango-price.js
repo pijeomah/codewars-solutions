c19e0bc036b7f319e54f3414c9e0345d697135a6
pijeomah <promiseij4919@gmail.com>
2023-02-07 10:48:23 +0100

    new challenge added

---
+++ 8kyu/mango-price.js
@@ -0,0 +1,21 @@
+// There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+
+// Examples
+// mango(2, 3) ==> 6    # 2 mangoes for $3 per unit = $6; no mango for free
+// mango(3, 3) ==> 6    # 2 mangoes for $3 per unit = $6; +1 mango for free
+// mango(5, 3) ==> 12   # 4 mangoes for $3 per unit = $12; +1 mango for free
+// mango(9, 5) ==> 30   # 6 mangoes for $5 per unit = $30; +3 mangoes for free
+
+
+//My solution
+
+function mango(quantity, price){
+    if(quantity > 2){
+      let realQuant = Math.floor(quantity / 3)
+    quantity = quantity - realQuant
+    return quantity * price
+    }else{
+      return quantity * price
+    }
+      
+  }
