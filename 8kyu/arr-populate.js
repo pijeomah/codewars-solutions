b550c3364d7deb42bd5d7d0a2d116a0a9b8e75b5
pijeomah <promiseij4919@gmail.com>
2023-03-31 11:02:17 +0100

    new challenge added

---
+++ 8kyu/arr-populate.js
@@ -0,0 +1,13 @@
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites 1,000 times.
+
+
+//My solution
+var websites = []
+
+for(let i = 0; i <= 999; i++){
+websites.push('codewars')
+ 
+  
+}
