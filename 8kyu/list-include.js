d1ae998d8ccbc66a0a8e9b6aeb0a18adc223bfb6
pijeomah <promiseij4919@gmail.com>
2023-04-29 03:16:38 +0100

    new challenge added

---
+++ 8kyu/list-include.js
@@ -0,0 +1,9 @@
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+//My solution
+
+function include(arr, item){
+    if(arr.includes(item)){
+       return true
+       }else{return false}
+  }
