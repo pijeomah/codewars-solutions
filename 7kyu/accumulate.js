c7a6ab9d5b6ce7647a4f509be2aa4e3f0693ee76
pijeomah <promiseij4919@gmail.com>
2023-06-01 14:41:15 +0100

    new challenge added

---
+++ 7kyu/accumulate.js
@@ -0,0 +1,15 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+//My Solution
+
+function accum(s) {
+	return [...s].map((el, index) =>{
+        return el.toLowerCase() + el.toLowerCase().repeat(read).join('-')
+     })
+}
