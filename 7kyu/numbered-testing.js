14f12c9053dabc25cc28025601293293cfc58025
pijeomah <promiseij4919@gmail.com>
2023-06-18 15:43:20 +0100

    new challenge added

---
+++ 7kyu/numbered-testing.js
@@ -0,0 +1,13 @@
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+//Solution
+var number=function(array){
+    return array.map((el,index)=> (index + 1) + ': ' + el)
+  }
