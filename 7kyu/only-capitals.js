636b74ae70cef1602d7bffe9a4c5f7f0678a27a0
pijeomah <promiseij4919@gmail.com>
2023-06-27 18:01:52 +0100

    new challenge added

---
+++ 7kyu/only-capitals.js
@@ -0,0 +1,18 @@
+// Instructions
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+//My solution
+var capitals = function (word) {
+    let wordArr = word.split('')
+    let empty = []
+    wordArr.filter((el,i)=> {
+      if(el !== el.toUpperCase()){
+        return el
+      }else if(el === el.toUpperCase()){
+        empty.push(i)
+      }
+    })
+    return empty
