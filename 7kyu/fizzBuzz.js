2e1a4a3d8adf1235ef69287961c1ae2394329ba8
pijeomah <promiseij4919@gmail.com>
2023-07-01 07:04:05 +0100

    new challenge added

---
+++ 7kyu/fizzBuzz.js
@@ -0,0 +1,33 @@
+// Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+// Replace certain values however if any of the following conditions are met:
+
+// If the value is a multiple of 3: use the value "Fizz" instead
+// If the value is a multiple of 5: use the value "Buzz" instead
+// If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+// N will never be less than 1.
+
+// Method calling example:
+
+// fizzbuzz(3) -->  [1, 2, "Fizz"]
+
+//My solution
+function fizzbuzz(n)
+{
+  let fbArray = []
+  for(let i = 1; i <= n; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+       fbArray.push("FizzBuzz")
+    }else if(i % 3 === 0){
+    fbArray.push("Fizz")
+    }else if(i % 5 === 0){
+        fbArray.push("Buzz")
+    }else if(i % 3 !== 0 || i % 5 !== 0){
+        fbArray.push(i)
+    }
+  
+  }
+  
+  return fbArray
+
+}
