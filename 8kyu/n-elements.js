881cf5baa583ac7599360eaf0f05401eed9e8fb6
pijeomah <promiseij4919@gmail.com>
2023-08-28 03:46:05 +0100

    new challenge added

---
+++ 8kyu/n-elements.js
@@ -0,0 +1,12 @@
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+// If you need help, here's a reference:
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+
+
+//My solution
+function take(arr, n) {
+    let filtered = arr.filter((el,i) => i < n )
+    return filtered
+  }
