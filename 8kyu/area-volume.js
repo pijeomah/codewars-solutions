bca47638f02f8dde9c7bfec768c9e1898be83dff
pijeomah <promiseij4919@gmail.com>
2023-09-07 03:56:51 +0100

    new challenge added

---
+++ 8kyu/area-volume.js
@@ -0,0 +1,9 @@
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+//My solution
+function getSize(width, height, depth){
+    const area =  (2 * depth * width) + (2 * depth * height) + (2 * height * width)
+    const volume = width * height * depth
+    return [area, volume]
+  }
+  
