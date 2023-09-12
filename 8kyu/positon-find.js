752e271b30cf52fd0620bb4b065d949f5a69639e
pijeomah <promiseij4919@gmail.com>
2023-09-12 04:00:04 +0100

    new challenge added

---
+++ 8kyu/positon-find.js
@@ -0,0 +1,12 @@
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+
+//My solution
+function position(letter){
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    return 'Position of alphabet: ' + (alphabet.indexOf(letter)+1)
+    }
