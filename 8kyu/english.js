86b4314c3f4dd7ed8aad65f5b70829f20da2f489
pijeomah <promiseij4919@gmail.com>
2023-09-18 02:02:48 +0100

    new challenge added

---
+++ 8kyu/english.js
@@ -0,0 +1,17 @@
+// Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+// The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+// Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+// Return value as boolean values, true for the string to contains "English", false for it does not.
+
+//My solution
+function spEng(sentence){
+    let newSentence = sentence.toLowerCase()
+  if (newSentence.match("english")){
+    return true
+  }else{
+    return false
+  }
+  }
