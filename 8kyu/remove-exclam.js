e8e667174ed91046742bc74a5f104271f8f6ac06
pijeomah <promiseij4919@gmail.com>
2023-09-26 02:10:30 +0100

    new challenge added

---
+++ 8kyu/remove-exclam.js
@@ -0,0 +1,16 @@
+// Description:
+// Remove all exclamation marks from the end of sentence.
+
+// Examples
+// "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
+
+
+//My solution
+function remove (string) {  
+    return string.replace(/!+$/, '')
+  }
